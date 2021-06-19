fca337de05a76efc3ce22a34d2710c605bcb08a3
gaeunek <l9960204@naver.com>
2021-06-19 14:38:58 +0900

    210619 | Update | 배준 union-find 문제 추가 백준 4195 친구 네트워크 문제 수정(풀이 O)

--- backjoon/unionfind/Main4195.java
+++ backjoon/unionfind/Main4195.java
@@ -4,19 +4,30 @@ import java.util.*;
 import java.io.*;
 
 public class Main4195 {
-	static int tc, f;
-	static Map<String, String> map;
+	static int tc, f, max;
+	static Map<String, Integer> map;
+	static int[] parent, count;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
 
 		tc = Integer.parseInt(br.readLine());
 
 		while (tc-- > 0) {
 			f = Integer.parseInt(br.readLine());
+			max = f * 2;
+			parent = new int[max];
+			count = new int[max];
 			map = new HashMap<>();
-			StringBuilder sb = new StringBuilder();
+
+			int index = 0;
+
+			for (int i = 0; i < parent.length; i++) {
+				parent[i] = i;
+				count[i] = 1;
+			}
 
 			for (int i = 0; i < f; i++) {
 				st = new StringTokenizer(br.readLine(), " ");
@@ -25,50 +36,39 @@ public class Main4195 {
 				String b = st.nextToken();
 
 				if (!map.containsKey(a))
-					map.put(a, a);
+					map.put(a, index++);
 				if (!map.containsKey(b))
-					map.put(b, b);
+					map.put(b, index++);
 
-				union(a, b);
-
-				sb.append(check(a, b)).append("\n");
+				sb.append(union(map.get(a), map.get(b))).append("\n");
 			}
-
-			System.out.println(sb.toString());
 		}
-	}
-
-	public static int check(String a, String b) {
-		Iterator<String> it = map.values().iterator();
-		int count = 0;
 		
-		while (it.hasNext()) {
-			String value = it.next();
+		System.out.println(sb.toString());
+	}
 
-			if (value.equals(a) || value.equals(b))
-				count++;
+	public static int union(int a, int b) {
+		int findA = find(a);
+		int findB = find(b);
+
+		if (findA == findB)
+			return count[findA];
+
+		if (findA < findB) {
+			parent[findB] = findA;
+			count[findA] += count[findB];
+			return count[findA];
+		} else {
+			parent[findA] = findB;
+			count[findB] += count[findA];
+			return count[findB];
 		}
-
-		return count;
 	}
 
-	public static void union(String a, String b) {
-		String findA = find(a);
-		String findB = find(b);
+	public static int find(int n) {
+		if (parent[n] == n)
+			return n;
 
-		if (findA.equals(findB))
-			return;
-
-		if (findA.compareTo(findB) < 1) {
-			map.put(findB, findA);
-		} else
-			map.put(findA, findB);
-	}
-
-	public static String find(String s) {
-		if (map.get(s) == s)
-			return s;
-
-		return map.put(s, map.get(s));
+		return parent[n] = find(parent[n]);
 	}
 }
